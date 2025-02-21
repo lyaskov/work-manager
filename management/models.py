e7dec76d6a206d2138cb13870bfdca3e77527bdb
lyaskov <lyaskov1@gmail.com>
2025-02-21 22:05:22 +0200

    Add initial models (Worker, Task, Position, TaskType) with migrations

--- management/models.py
+++ management/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position, on_delete=models.CASCADE, related_name='workers'
+    )
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=20, choices=[
+        ('Urgent', 'Urgent'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low')
+    ])
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name='tasks')
+
+    def __str__(self):
+        return self.name
--- work_manager/settings.py
+++ work_manager/settings.py
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "management.Worker"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
