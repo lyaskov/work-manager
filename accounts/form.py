4a62288013dfe5753436a34e874b63c147bfac09
lyaskov <lyaskov1@gmail.com>
2025-03-03 00:31:05 +0200

    Add templates for registration and login pages

--- accounts/form.py
+++ accounts/form.py
@@ -14,9 +14,35 @@ class WorkerRegistrationForm(UserCreationForm):
         self.fields['password1'].widget.attrs.pop('aria-describedby', None)
         self.fields['password2'].widget.attrs.pop('aria-describedby', None)
 
-    first_name = forms.CharField(max_length=150, required=True)
-    last_name = forms.CharField(max_length=150, required=True)
-    position = forms.ModelChoiceField(queryset=Position.objects.all(), required=True)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Password Confirmation'
+
+    first_name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="First name",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'First name'}
+        ),
+    )
+    last_name = forms.CharField(
+        max_length=150,
+        required=True,
+        label="Last name",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Last name'}
+        ),
+    )
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        required=True,
+        widget=forms.Select(
+            attrs={'class': 'form-select'}
+        ),
+        empty_label="Select Position"
+    )
 
     class Meta:
         model = Worker
@@ -34,5 +60,12 @@ class WorkerRegistrationForm(UserCreationForm):
             'password1': None,
             'password2': None,
         }
-
+        widgets = {
+            'username': forms.TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Username'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control', 'placeholder': 'Email'}
+            ),
+        }
 
--- management/urls.py
+++ management/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from management.views import index, dashboard
 
 urlpatterns = [
-    path("", index, name="index"),
     path("dashboard/", dashboard, name="dashboard"),
 ]
 app_name = "management"
