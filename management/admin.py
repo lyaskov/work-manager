77c170fda814d4eea4eb192220f153b455636370
lyaskov <lyaskov1@gmail.com>
2025-03-03 19:45:52 +0200

    Add TaskType control via admin panel

--- management/admin.py
+++ management/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from management.models import Position
+from management.models import Position, TaskType
 
 
 # Register your models here.
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
