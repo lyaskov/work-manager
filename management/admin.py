4ac48b2d5dfc431d50b5cbd3069c1e4e97489fe3
lyaskov <lyaskov1@gmail.com>
2025-03-03 19:05:55 +0200

    Add the ability to manage positions via the admin panel

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from management.models import Position
+
+
 # Register your models here.
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
