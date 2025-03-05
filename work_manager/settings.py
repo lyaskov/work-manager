b6be79a016c747be38875e01f8493a71f48a1a0a
lyaskov <lyaskov1@gmail.com>
2025-03-05 12:21:02 +0200

    Move SECRET_KEY to environment variables for improved security

--- work_manager/settings.py
+++ work_manager/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.core.exceptions import ImproperlyConfigured
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--amui%#tocw-iyp51_szf69p8&w0qn6mt6g&kdg$7vn(0bm13$'
-
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+if not SECRET_KEY:
+    raise ImproperlyConfigured("SECRET_KEY is not set in environment variables")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
